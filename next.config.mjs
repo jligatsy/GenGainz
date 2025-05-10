95a5cb466f07843df8a0910d1fa468f18c739c09
Josephine Ligatsyah <jligatsy@uci.edu>
2025-05-10 12:29:20 -0700

    updated package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',  // Add this line to enable static export
+  output: 'standalone',  // Use standalone to support SSR
 };
 
 export default nextConfig;
